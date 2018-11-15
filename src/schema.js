2afe29ac6eb3fb9de2dc8106e6089ef8f809775f
Emanuele Liardo <emanuele.liardo@fleetmatica.com>
2018-11-15 17:46:15 +0100

    feat (schema): idPaese

--- src/schema.js
+++ src/schema.js
@@ -2,10 +2,14 @@
 
 const Joi = require('joi')
 
-const FatturaElettronicaHeaderSchema = Joi.object().keys({
-  DatiTrasmissione: Joi.object().keys({
-    IdTrasmittente: Joi.object().keys().required()
+const DatiTrasmissioneSchema = Joi.object().keys({
+  IdTrasmittente: Joi.object().keys({
+    IdPaese: Joi.string().required().length(2)
   }).required()
+}).required()
+
+const FatturaElettronicaHeaderSchema = Joi.object().keys({
+  DatiTrasmissione: DatiTrasmissioneSchema
 }).required()
 
 module.exports = Joi.object().keys({
--- tests/schema.test.js
+++ tests/schema.test.js
@@ -45,6 +45,61 @@ describe('Schema', () => {
           } = result || {}
           expect(details.some(e => e.message === '"IdTrasmittente" is required')).toBeTruthy()
         })
+
+        test('should require IdPaese', () => {
+          const value = {
+            FatturaElettronicaHeader: {
+              DatiTrasmissione: {
+                IdTrasmittente: {}
+              }
+            }
+          }
+          const result = Joi.validate(value, schema)
+          const {
+            error: {
+              details = []
+            } = {}
+          } = result || {}
+          expect(details.some(e => e.message === '"IdPaese" is required')).toBeTruthy()
+        })
+
+        test('IdPaese should be a string', () => {
+          const value = {
+            FatturaElettronicaHeader: {
+              DatiTrasmissione: {
+                IdTrasmittente: {
+                  IdPaese: 1
+                }
+              }
+            }
+          }
+          const result = Joi.validate(value, schema)
+          const {
+            error: {
+              details = []
+            } = {}
+          } = result || {}
+          expect(details.some(e => e.message === '"IdPaese" must be a string')).toBeTruthy()
+        })
+
+        test('"IdPaese" length must be 2 characters long', () => {
+          const value = {
+            FatturaElettronicaHeader: {
+              DatiTrasmissione: {
+                IdTrasmittente: {
+                  IdPaese: 'ITT'
+                }
+              }
+            }
+          }
+          const result = Joi.validate(value, schema)
+          const {
+            error: {
+              details = []
+            } = {}
+          } = result || {}
+          expect(details.some(e => e.message === '"IdPaese" length must be 2 characters long')).toBeTruthy()
+        })
       })
     })
   })
