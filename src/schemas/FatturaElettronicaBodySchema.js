a2e3c8cf821cf96e8071f0f1e91765bdcb16402c
Emanuele Liardo <emanuele.liardo@fleetmatica.com>
2018-11-16 17:42:03 +0100

    feat (schema): body 2.1.9.*

--- src/schemas/FatturaElettronicaBodySchema.js
+++ src/schemas/FatturaElettronicaBodySchema.js
@@ -7,6 +7,14 @@ const TipiRitenuteValide = ['RT01', 'RT02']
 const TipiCassaValidi = ['TC01', 'TC02', 'TC03', 'TC04', 'TC05', 'TC06', 'TC07', 'TC08', 'TC09', 'TC10', 'TC11', 'TC12', 'TC13', 'TC14', 'TC15', 'TC16', 'TC17', 'TC18', 'TC19', 'TC20', 'TC21', 'TC22']
 const NaturaValidi = ['N1', 'N2', 'N3', 'N4', 'N5', 'N6', 'N7']
 
+const IdPaeseSchema = Joi.string().uppercase().length(2)
+const IdCodiceSchema = Joi.string().alphanum().min(2).max(28)
+const IdFiscaleIVASchema = Joi.object().keys({
+  IdPaese: IdPaeseSchema.required(),
+  IdCodice: IdCodiceSchema.required()
+})
+const CodiceFiscaleSchema = Joi.string().alphanum().min(11).max(16)
+
 const DatiRitenutaSchema = Joi.object().keys({
   TipoRitenuta: Joi.valid(TipiRitenuteValide).required(),
   ImportoRitenuta: Joi.number().required(), // min 4 max 15
@@ -107,6 +115,44 @@ const DatiDDTSchema = Joi.alternatives().try(
   Joi.array().items(DatiDDTItemSchema),
   DatiDDTItemSchema)
 
+const DatiAnagraficiVettoreSchema = Joi.object().keys({
+  IdFiscaleIVA: IdFiscaleIVASchema.required(), // 2.1.9.1.1
+  CodiceFiscale: CodiceFiscaleSchema, // 2.1.9.1.2
+  Anagrafica: Joi.object().keys({
+    Denominazione: Joi.string().alphanum().min(1).max(80),
+    Nome: Joi.string().alphanum().min(1).max(60),
+    Cognome: Joi.string().alphanum().min(1).max(60),
+    Titolo: Joi.string().min(2).max(10),
+    CodEORI: Joi.string().min(13).max(17)
+  }).required(), // 2.1.9.1.3
+  NumeroLicenzaGuida: Joi.string().min(1).max(20) // 2.1.9.1.4
+})
+
+const IndirizzoResaSchema = Joi.object().keys({
+  Indirizzo: Joi.string().alphanum().min(1).max(60).required(),
+  NumeroCivico: Joi.string().alphanum().min(1).max(8),
+  CAP: Joi.string().regex(/^\d{5}$/).required(),
+  Comune: Joi.string().min(1).max(60).required(),
+  Provincia: Joi.string().uppercase().length(2),
+  Nazione: Joi.string().uppercase().length(2).required()
+})
+
+const DatiTrasportoSchema = Joi.object().keys({
+  DatiAnagraficiVettore: DatiAnagraficiVettoreSchema, // 2.1.9.1
+  MezzoTrasporto: Joi.string().min(1).max(80), // 2.1.9.2
+  CausaleTrasporto: Joi.string().min(1).max(100), // 2.1.9.3
+  NumeroColli: Joi.number().integer().min(1).max(9999), // 2.1.9.4
+  Descrizione: Joi.string().min(1).max(100), // 2.1.9.5
+  UnitaMisuraPeso: Joi.string().min(1).max(10), // 2.1.9.6
+  PesoLordo: Joi.number().min(0), // 2.1.9.7
+  PesoNetto: Joi.number().min(0), // 2.1.9.8
+  DataOraRitiro: Joi.string().isoDate().raw().length(19), // 2.1.9.9
+  DataInizioTrasporto: Joi.string().isoDate().raw().length(10), // 2.1.9.10
+  TipoResa: Joi.string().length(3), // 2.1.9.11
+  IndirizzoResa: IndirizzoResaSchema, // 2.1.9.12
+  DataOraConsegna: Joi.string().isoDate().raw().length(19) // 2.1.9.13
+})
+
 const DatiGeneraliSchema = Joi.object().keys({
   DatiGeneraliDocumento: DatiGeneraliDocumentoSchema.required(), // 2.1.1
   DatiOrdineAcquisto: DatiOrdineAcquistoSchema, // 2.1.2
@@ -115,7 +161,8 @@ const DatiGeneraliSchema = Joi.object().keys({
   DatiRicezione: DatiOrdineAcquistoSchema, // 2.1.5
   DatiFattureCollegate: DatiOrdineAcquistoSchema, // 2.1.6
   DatiSAL: DatiSALSchema, // 2.1.7
-  DatiDDT: DatiDDTSchema // 2.1.8
+  DatiDDT: DatiDDTSchema, // 2.1.8
+  DatiTrasporto: DatiTrasportoSchema // 2.1.9
 }).required()
 
 const FatturaElettronicaBodyItemSchema = Joi.object().keys({
