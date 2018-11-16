ef8c2d8915803ae3aac58552aeea642af1a4b3fe
Emanuele Liardo <emanuele.liardo@fleetmatica.com>
2018-11-16 10:16:51 +0100

    feat (schema): required fields in DatiTrasmissioneSchema

--- src/schema.js
+++ src/schema.js
@@ -5,7 +5,14 @@ const Joi = require('joi')
 const DatiTrasmissioneSchema = Joi.object().keys({
   IdTrasmittente: Joi.object().keys({
     IdPaese: Joi.string().required().length(2),
-    IdCodice: Joi.string().required().min(2).max(28)
+    IdCodice: Joi.string().alphanum().required().min(2).max(28)
+  }).required(),
+  ProgressivoInvio: Joi.string().alphanum().required().min(1).max(10),
+  FormatoTrasmissione: Joi.valid('FPA12', 'FPR12').required(),
+  CodiceDestinatario: Joi.alternatives().when('FormatoTrasmissione', {
+    is: 'FPA12',
+    then: Joi.string().alphanum().length(6),
+    otherwise: Joi.string().alphanum().length(7)
   }).required()
 }).required()
 
