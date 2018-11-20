4cb401b43bbebce1a82fe68d2347126af7aeb92a
Emanuele Liardo <emanuele.liardo@fleetmatica.com>
2018-11-20 10:00:14 +0100

    test: fattura pa

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,9 +1,10 @@
 'use strict'
 
 const json2fatturapaxml = require('../index')
+const fs = require('fs')
 
 describe('json2fatturapaxml', () => {
-  test('should generate xml for FPR12', () => {
+  test('should generate minimal xml for FPR12', () => {
     const invoice = {
       FatturaElettronicaHeader: {
         DatiTrasmissione: {
@@ -77,7 +78,86 @@ describe('json2fatturapaxml', () => {
         }
       }
     }
-    const expected = require('fs').readFileSync('tests/xml/IT03469550986_FPR001.xml', { encoding: 'UTF-8' })
+    const expected = fs.readFileSync('tests/xml/IT03469550986_FPR001.xml', { encoding: 'UTF-8' })
+    const result = json2fatturapaxml(invoice)
+    expect(result.error).toBeUndefined()
+    expect(result).toEqual(expected)
+  })
+
+  test('should generate minimal xml for FPR12', () => {
+    const invoice = {
+      FatturaElettronicaHeader: {
+        DatiTrasmissione: {
+          IdTrasmittente: {
+            IdPaese: 'IT',
+            IdCodice: '03469550986'
+          },
+          ProgressivoInvio: '001',
+          FormatoTrasmissione: 'FPA12',
+          CodiceDestinatario: 'GIQELA'
+        },
+        CedentePrestatore: {
+          DatiAnagrafici: {
+            IdFiscaleIVA: {
+              IdPaese: 'IT',
+              IdCodice: '03469550986'
+            },
+            Anagrafica: {
+              Denominazione: 'dfa'
+            },
+            RegimeFiscale: 'RF19'
+          },
+          Sede: {
+            Indirizzo: 'Indirizzo',
+            CAP: '00000',
+            Comune: 'Comune',
+            Nazione: 'IT'
+          }
+        },
+        CessionarioCommittente: {
+          DatiAnagrafici: {
+            IdFiscaleIVA: {
+              IdPaese: 'IT',
+              IdCodice: '03469550986'
+            },
+            Anagrafica: {
+              Denominazione: 'Denominazione'
+            }
+          },
+          Sede: {
+            Indirizzo: 'Indirizzo',
+            CAP: '00000',
+            Comune: 'Comune',
+            Nazione: 'IT'
+          }
+        }
+      },
+      FatturaElettronicaBody: {
+        DatiGenerali: {
+          DatiGeneraliDocumento: {
+            TipoDocumento: 'TD01',
+            Divisa: 'EUR',
+            Data: '2018-11-19',
+            Numero: '1'
+          }
+        },
+        DatiBeniServizi: {
+          DettaglioLinee: {
+            NumeroLinea: 1,
+            Descrizione: 'Descrizione',
+            PrezzoUnitario: '0.00',
+            PrezzoTotale: '0.00',
+            AliquotaIVA: '22.00'
+          },
+          DatiRiepilogo: {
+            AliquotaIVA: '22.00',
+            ImponibileImporto: '0.00',
+            Imposta: '0.00'
+          }
+        }
+      }
+    }
+    const expected = fs.readFileSync('tests/xml/IT03469550986_PA001.xml', { encoding: 'UTF-8' })
     const result = json2fatturapaxml(invoice)
     expect(result.error).toBeUndefined()
     expect(result).toEqual(expected)
